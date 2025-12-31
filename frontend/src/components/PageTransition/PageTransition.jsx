918334fa3c44e8f96cf538692d5e18a9e3aa77f3
Ashique <ashique9496@gmail.com>
2025-12-31 19:05:04 +0530

    Password validation completed

--- frontend/src/components/PageTransition/PageTransition.jsx
+++ frontend/src/components/PageTransition/PageTransition.jsx
@@ -5,15 +5,12 @@ import { motion } from "framer-motion";
 const pageVariants = {
     initial: {
         opacity: 0,
-        y: 20,
     },
     animate: {
         opacity: 1,
-        y: 0,
     },
     exit: {
         opacity: 0,
-        y: -20,
     },
 };
 
