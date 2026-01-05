54cf52e421584dcd3ae98366be990f2a0c798a9c
Ashique <ashique9496@gmail.com>
2026-01-05 10:11:18 +0530

    The duplicating issue is solved

--- frontend/src/components/ClientLayout/ClientLayout.jsx
+++ frontend/src/components/ClientLayout/ClientLayout.jsx
@@ -1,16 +1,6 @@
 "use client";
-import { AnimatePresence } from "framer-motion";
-import { usePathname } from "next/navigation";
 import SmoothScroll from "@/components/SmoothScroll/SmoothScroll";
 
 export default function ClientLayout({ children }) {
-    const pathname = usePathname();
-
-    return (
-        <SmoothScroll>
-            <AnimatePresence mode="sync" initial={false}>
-                <div key={pathname}>{children}</div>
-            </AnimatePresence>
-        </SmoothScroll>
-    );
+    return <SmoothScroll>{children}</SmoothScroll>;
 }
